e9adee928dd0797255984ba632b4ea294f6f2d74
19ucs020 <54469240+19ucs020@users.noreply.github.com>
2019-10-12 17:40:24 +0530

    changes

---
+++ Hcfest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+void main() 
+{
+    int i,j;
+    for(i=1;i<=5;i=i+1)
+        {for(j=1;j<=i;j=j+1)
+            printf("*");
+        printf("\n");
+        }
+    
+}
